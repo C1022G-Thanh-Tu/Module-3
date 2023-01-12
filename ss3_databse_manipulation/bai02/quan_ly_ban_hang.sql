5e435787277e4b3c0c856e2d1676b49439780e5f
thanhtu2607 <nthtu1998@gmail.com>
2023-01-12 17:12:29 +0700

    update

--- ss3_databse_manipulation/bai02/quan_ly_ban_hang.sql
+++ ss3_databse_manipulation/bai02/quan_ly_ban_hang.sql
@@ -12,3 +12,7 @@ select customer.customer_name from customer
 left join `order` on customer.customer_id = `order`.customer_id
 where `order`.customer_id is null;
 
+select `order`.order_id, `order`.order_date, order_detail.order_quantity*product.product_price as order_total_price 
+from order_detail
+inner join `order` on order_detail.order_id = `order`.order_id
+inner join product on order_detail.product_id = product.product_id;
