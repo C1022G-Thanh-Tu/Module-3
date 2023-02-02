e1705372d0ce9b0e98d020a8c8b13992caba54ac
thanhtu2607 <nthtu1998@gmail.com>
2023-02-02 16:56:23 +0700

    update

--- case_study_database/sql_6_10.sql
+++ case_study_database/sql_6_10.sql
@@ -4,11 +4,11 @@ use furama_database;
 -- dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
 select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, 
 dv.chi_phi_thue, ldv.ten_loai_dich_vụ 
-from hop_dong hd
-join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+from dich_vu dv
 join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
-where quarter(hd.ngay_lam_hop_dong) <> 1 
-and year(hd.ngay_lam_hop_dong) = 2021
+where dv.ma_dich_vu not in (select ma_dich_vu from hop_dong hd 
+where quarter(hd.ngay_lam_hop_dong) = 1 
+and year(hd.ngay_lam_hop_dong) = 2021)
 group by dv.ma_dich_vu;
 
 -- Câu 7: Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
