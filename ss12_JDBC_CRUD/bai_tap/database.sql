d65b124819154f557a8367c94eca15ee8988f7ff
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-02-14 22:20:34 +0700

    update

--- ss12_JDBC_CRUD/bai_tap/database.sql
+++ ss12_JDBC_CRUD/bai_tap/database.sql
@@ -11,4 +11,17 @@ insert into users (name, email, country) values
 ('Minh','minh@codegym.vn','Viet Nam'),
 ('Kante','kante@che.uk','Kenia');
 
-select id, name, email, country from users;
+select id, name, email, country from users order by name;
+
+insert into users (name, email, country) values (?,?,?);
+
+select id, name, email, country from users where id =? order by name;
+
+set sql_safe_updates = 0;
+update users
+set name = ?, email = ?, country = ? 
+where id = ?;
+
+delete from users where id = ?;
+
+select id, name, email, country from users where country like ? order by name;
